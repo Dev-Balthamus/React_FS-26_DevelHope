7d70a65bbe02648ad0516a1ad27ab9e90d3d0f6b
Francesco <francesco7411@gmail.com>
2024-06-07 22:12:39 +0200

    Add '04-Effect 2nd' ex

---
+++ 04-Effect 2nd/App.jsx
@@ -0,0 +1,26 @@
+import { AlertClock } from "./AlertClock";
+import { Clock } from "./Clock";
+import { Counter } from "./Counter";
+import { Hello } from "./Hello";
+import { Welcome } from "./Welcome";
+
+export function App() {
+  function handleShowTime() {
+    const timeNow = new Date();
+    alert(`The current time is:   ${timeNow.toLocaleTimeString()}`);
+  }
+
+  return (
+    <div>
+      <Hello />
+      <hr />
+      <Welcome name={"Francesco"} age={12} />
+      <hr />
+      <AlertClock onClick={handleShowTime} />
+      <hr />
+      <Counter initialValue={4} increment={3} decrement={2} />
+      <hr />
+      <Clock />
+    </div>
+  );
+}
